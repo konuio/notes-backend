1d203db693500c597989359450362a74034835f6
Michael K Hsueh <michael.k.hsueh@gmail.com>
2015-11-23 02:38:15 -0800

    replace handler with new buddy handler

--- src/konu_notes/app_state.clj
+++ src/konu_notes/app_state.clj
@@ -1,15 +1,4 @@
 (ns konu-notes.app-state
   )
 
-
-;; Global storage for store generated tokens.
-(def tokens (atom {}))
-
-(defn get-tokens-state []
-  @tokens)
-
-;; TODO refactor to use mongo instead with TTL
-(defn add-token [token username]
-  (swap! tokens assoc (keyword token) (keyword username)))
-
-;; TODO refactor tokens to use api instead of direct access.
+;; Store atoms here.
