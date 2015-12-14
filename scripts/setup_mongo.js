9e3e3f026b3ed47542ea5245ff06b236abee21e9
Matthew King <mking@users.noreply.github.com>
2015-12-13 19:04:52 -0800

    cap staged_users

--- scripts/setup_mongo.js
+++ scripts/setup_mongo.js
@@ -1,13 +1,16 @@
 // mongo development scripts/setup_mongo.js
 
 // Session expiration index
-db['session-tokens'].createIndex({lastActive: 1}, {expireAfterSeconds: 21600})
+db.session_tokens.createIndex({lastActive: 1}, {expireAfterSeconds: 21600})
 
 // Index for testing
-// db['session-tokens'].dropIndex({lastActive: 1}); db['session-tokens'].createIndex({lastActive: 1}, {expireAfterSeconds: 10})
+// db.session_tokens.dropIndex({lastActive: 1}); db.session_tokens.createIndex({lastActive: 1}, {expireAfterSeconds: 10})
 
 // Disallow duplicate emails
 db.users.createIndex({email: 1}, {unique: true})
 
 // Disallow duplicate usernames
 db.users.createIndex({username: 1}, {unique: true})
+
+// staged_users capped collection
+db.createCollection('staged_users', {capped: true, size: 5120000, max: 5000})
